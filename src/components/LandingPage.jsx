db588574842b35ed5b1c162f2deb67c9344f6833
DevarathN9 <devarath.nediyirippil@lejhro.com>
2025-08-15 20:36:55 +0530

    initial commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,50 @@
+const { useState } = require("react");
+
+const LandingPage = () => {
+  const [inputText, setInputText] = useState("");
+  const [suggestedText, setSuggestedText] = useState(null);
+  const customDictionary = {
+    teh: "the",
+
+    wrok: "work",
+
+    fot: "for",
+
+    exampl: "example",
+  };
+  const handleInputChange = (text) => {
+    setInputText(text);
+    const words = inputText.split(" ");
+    const correctedWords = words.map((word, index) => {
+      const correctedWord = customDictionary[word.toLowerCase()];
+      return correctedWord || word;
+    });
+    const firstCorrection = correctedWords.find(
+      (word, index) => word !== words[index]
+    );
+    setSuggestedText(firstCorrection);
+  };
+
+  return (
+    <div>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea
+        name=""
+        id=""
+        value={inputText}
+        onChange={(e) => {
+          handleInputChange(e.target.value);
+        }}
+        rows={5}
+        cols={40}
+        placeholder="Enter text..."
+      ></textarea>
+      {suggestedText && (
+        <p>
+          Did you mean: <strong>{suggestedText}?</strong>
+        </p>
+      )}
+    </div>
+  );
+};
+export default LandingPage;
